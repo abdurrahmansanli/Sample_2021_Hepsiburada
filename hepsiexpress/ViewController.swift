95dc9b877ed5b576b16c85caa716cf3f20988924
Abdurrahman Şanlı <abdurrahman.sanli@appcent.mobi>
2021-05-21 20:42:35 +0300

    Base Xcode project files.

---
+++ hepsiexpress/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  hepsiexpress
+//
+//  Created by Abdurrahman Şanlı on 21.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
